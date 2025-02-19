7b91f65e055f2a0139ae00fe9485a3015d2a96c0
Guillermo <guillermo.lopzap@gmail.com>
2025-02-19 22:19:59 +0100

    arreglo doc p0

--- Practica_0/doc/type-search-index.js
+++ Practica_0/doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"es.ubu.inf.edat.pr00","l":"EstadisticasArray"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"es.ubu.inf.edat.pr00","l":"EstadisticasArray"},{"p":"es.ubu.inf.edat.pr00","l":"EstadisticasArrayTest"}];updateSearchResults();
