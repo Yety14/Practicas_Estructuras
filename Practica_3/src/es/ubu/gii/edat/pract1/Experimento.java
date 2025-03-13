8d426e06b915603939fbd6b211d17d675b39bd0a
MariaGV-05 <maria.gv20005@gmail.com>
2025-03-13 09:17:18 +0100

    Comentarios en experimento

--- Practica_3/src/es/ubu/gii/edat/pract1/Experimento.java
+++ Practica_3/src/es/ubu/gii/edat/pract1/Experimento.java
@@ -3,8 +3,23 @@ package es.ubu.gii.edat.pract1;
 import es.ubu.gii.edat.datos.GeneradorEnteros;
 import java.util.Scanner;
 
+/**
+ * Clase Experimento. Permite introducir el número de valores y comprobar cuánto
+ * tiempo dura en ordenadorlos con ambos métodos de ordenación.
+ * 
+ * @author <a href="mgv1029@alu.ubu.es">María Guzmán Valdezate</a>
+ * @author <a href="glz1001@alu.ubu.es">Guillermo López de Arechavaleta
+ *         Zapatero</a>
+ * @since 1.0
+ * @version 1.0.1
+ */
 public class Experimento {
-
+	/**
+	 * Método principal que ejecuta el experimento de comparación de tiempos de
+	 * búsqueda del máximo utilizando dos métodos.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// Try-with-resources para cerrar automáticamente el Scanner
 		try (Scanner escaner = new Scanner(System.in)) {
@@ -23,7 +38,7 @@ public class Experimento {
 				coleccion.findMaxElement();
 				long t1_f = System.currentTimeMillis();
 				System.out.println("Tiempo de buscar mayor en milisegundos: " + (t1_f - t1_i));
-				
+
 				System.out.println("Quieres seguir evaluando  Y/N");
 				String letra = escaner.next();
 				if (!letra.equalsIgnoreCase("Y")) {
