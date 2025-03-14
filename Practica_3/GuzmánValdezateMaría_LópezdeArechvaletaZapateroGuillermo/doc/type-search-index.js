9544ee2cb27e8c860fab256c97b4984f4b7d69e4
Guillermo <guillermo.lopzap@gmail.com>
2025-03-14 17:42:26 +0100

    listo

--- Practica_3/GuzmánValdezateMaría_LópezdeArechvaletaZapateroGuillermo/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"es.ubu.gii.edat.pract1","l":"Experimento"},{"p":"es.ubu.gii.edat.datos","l":"GeneradorEnteros"},{"p":"es.ubu.gii.edat.pract1","l":"MaxElementCollection"},{"p":"es.ubu.gii.edat.pract1","l":"MaxElementCollectionTest"}];updateSearchResults();
