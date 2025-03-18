85d8125aa26b37578e9ca448f4c16180fdeb561b
Guillermo <guillermo.lopzap@gmail.com>
2025-03-18 18:58:38 +0100

    aaaa

---
+++ Práctica_5/src/es/ubu/inf/edat/pract02/ColaCircularEnlazada.java
@@ -0,0 +1,11 @@
+package es.ubu.inf.edat.pract02;
+
+import java.util.AbstractQueue;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+
+public class ColaCircularEnlazada<T> extends AbstractQueue<T> {
+	
+
+}
