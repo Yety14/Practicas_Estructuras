c9d40bf41241b74a001aff679ea7122d576b2531
Guillermo <guillermo.lopzap@gmail.com>
2025-04-29 19:47:02 +0200

    Update ConjuntoLRU.java

--- Practica_9/src/es/ubu/gii/edat/pr4/ConjuntoLRU.java
+++ Practica_9/src/es/ubu/gii/edat/pr4/ConjuntoLRU.java
@@ -9,17 +9,16 @@ import es.ubu.gii.edat.utils.cacheLRUEnlazada;
 
 public class ConjuntoLRU<E> extends AbstractSet<E> implements SortedSet<E> {
 
-
 	private int capacidad;
 	private int contador;
 	private cacheLRUEnlazada<E, E> mapa;
 	private int inicial = 0;
 	private int fin = 0;
-    
+
 	public ConjuntoLRU() {
-        throw new UnsupportedOperationException("Constructor por defecto no permitido.");
-    }
-	
+		throw new UnsupportedOperationException("Constructor por defecto no permitido.");
+	}
+
 	public ConjuntoLRU(int maxSize) {
 		this.capacidad = maxSize;
 		this.mapa = new cacheLRUEnlazada<E, E>(capacidad);
@@ -38,7 +37,6 @@ public class ConjuntoLRU<E> extends AbstractSet<E> implements SortedSet<E> {
 						}
 					}
 				}
-				mapa.remove();
 			}
 		}
 		if (mapa.containsKey(e)) {
@@ -65,57 +63,109 @@ public class ConjuntoLRU<E> extends AbstractSet<E> implements SortedSet<E> {
 	public int size() {
 		return mapa.size();
 	}
-	
-	@Override
-	public SortedSet<E> subSet(E a, E b) {
-		// TODO Auto-generated method stub
-        return null;	
-	}
-	
+
 	private E encontrar(int i) {
-        Iterator<E> it = iterator();
-        int contador = 0;
-        while (it.hasNext()) {
-            E e = it.next();
-            if (contador == i) {
-                return e;
-            }
-            contador++;
-        }
-        return null;
-	}
-	
-	@Override
-	 public E first(){
-		return encontrar(inicial); 
-	 }
-	
-	@Override
-	 public E last(){
-		return encontrar(fin); 
-	 }
-	
-	@Override
-    public SortedSet<E> tailSet(E fromElement){
+		Iterator<E> it = iterator();
+		int contador = 0;
+		while (it.hasNext()) {
+			E e = it.next();
+			if (contador == i) {
+				return e;
+			}
+			contador++;
+		}
 		return null;
 	}
-    
-    @Override
-    public Comparator<? super E> comparator(){
-    	return null;
-    }
-
 
 	@Override
+	public E first() {
+	    Iterator<E> it = iterator();
+	    if (!it.hasNext()) throw new IllegalStateException("Conjunto vacío.");
+	    return it.next(); // Primer accedido (menos reciente)
+	}
+
+	@Override
+	public E last() {
+	    Iterator<E> it = iterator();
+	    E last = null;
+	    while (it.hasNext()) {
+	        last = it.next();
+	    }
+	    if (last == null) throw new IllegalStateException("Conjunto vacío.");
+	    return last; // Más recientemente accedido
+	}
+
+	
+
+	/*
+	 * obtener los elemento que se han accedido por última vez hace más tiempo que
+	 * el que se facilita como parámetro
+	 */
+	@Override
 	public SortedSet<E> headSet(E toElement) {
-		// TODO Auto-generated method stub
+		if (!this.contains(toElement)) {
+		    throw new IllegalArgumentException("Elemento no presente en el conjunto.");
+		}
+		SortedSet<E> subConjunto = new ConjuntoLRU<>(this.capacidad);
+		E actual = encontrar(inicial);
+		Iterator<E> iterador = this.iterator();
+		while (iterador.hasNext()) {
+			if (actual == toElement) {
+				break;
+			}
+			subConjunto.add(actual);
+			actual = iterador.next();
+		}
+		return subConjunto;
+	}
+	
+	/*
+	 * obtener los elemento que se han accedido por última vez hace menos tiempo que
+	 * el que se facilita como parámetro
+	 */
+	@Override
+	public SortedSet<E> tailSet(E toElement) {
+		if (!this.contains(toElement)) {
+		    throw new IllegalArgumentException("Elemento no presente en el conjunto.");
+		}
+		SortedSet<E> subConjunto = new ConjuntoLRU<>(this.capacidad);
+		E actual = encontrar(fin);
+		Iterator<E> iterador = this.iterator();
+		while (iterador.hasNext()) {
+			if (actual == toElement) {
+				break;
+			}
+			subConjunto.add(actual);
+			actual = iterador.next();
+		}
+		return subConjunto;
+	}
+
+	@Override
+	public SortedSet<E> subSet(E a, E b) {
+		if (!this.contains(a) || !this.contains(b)) {
+		    throw new IllegalArgumentException("Elemento no presente en el conjunto.");
+		}
+		SortedSet<E> subConjunto = new ConjuntoLRU<>(this.capacidad);
+		Iterator<E> iterador = this.iterator();
+		while (iterador.hasNext()) {
+			E actual = iterador.next();
+			if (actual.equals(a) && actual.equals(b)) {
+				break;
+			}
+			subConjunto.add(actual);
+		}
+		return subConjunto;
+	}
+
+	@Override
+	public Comparator<? super E> comparator() {
 		return null;
 	}
 
 	@Override
 	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return mapa.values().iterator(); 		
 	}
 
 }
