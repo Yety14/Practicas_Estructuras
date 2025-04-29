f92ba766c412a290ea7f0496734cc4de4c1e4593
Guillermo <guillermo.lopzap@gmail.com>
2025-04-29 19:16:57 +0200

    Update ConjuntoLRU.java

--- Practica_9/src/es/ubu/gii/edat/pr4/ConjuntoLRU.java
+++ Practica_9/src/es/ubu/gii/edat/pr4/ConjuntoLRU.java
@@ -1,41 +1,48 @@
 package es.ubu.gii.edat.pr4;
+
 import java.util.AbstractSet;
+import java.util.SortedSet;
 import java.util.Comparator;
 import java.util.Iterator;
-import java.util.SortedSet;
 
 import es.ubu.gii.edat.utils.cacheLRUEnlazada;
 
 public class ConjuntoLRU<E> extends AbstractSet<E> implements SortedSet<E> {
 
-	private static final long serialVersionUID = 1L;
 
-	protected int MAXCAPACITY;
-	protected int MAXELEMENT;
-
-	protected cacheLRUEnlazada<E, E> control;
-
+	private int capacidad;
+	private int contador;
+	private cacheLRUEnlazada<E, E> mapa;
+	private int inicial = 0;
+	private int fin = 0;
+    
+	public ConjuntoLRU() {
+        throw new UnsupportedOperationException("Constructor por defecto no permitido.");
+    }
+	
 	public ConjuntoLRU(int maxSize) {
 		super();
-		this.MAXCAPACITY = maxSize;
-		this.control = new cacheLRUEnlazada<E, E>(MAXCAPACITY);
+		this.capacidad = maxSize;
+		this.mapa = new cacheLRUEnlazada<E, E>(capacidad);
+		contador = 0;
 	}
 
 	@Override
 	public boolean add(E e) {
-		if (control.containsKey(e)) {
+		if (mapa.containsKey(e)) {
 			return false;
 		}
 
-		control.put(e, e);
-
+		mapa.put(e, e);
+		contador++;
 		return true;
 	}
 
 	@Override
 	public boolean remove(Object o) {
-		if (control.containsKey(o)) {
-			control.remove(o);
+		if (mapa.containsKey(o)) {
+			mapa.remove(o);
+			contador--;
 			return true;
 		}
 
@@ -44,7 +51,7 @@ public class ConjuntoLRU<E> extends AbstractSet<E> implements SortedSet<E> {
 
 	@Override
 	public int size() {
-		return control.size();
+		return mapa.size();
 	}
 	
 	@Override
@@ -53,14 +60,27 @@ public class ConjuntoLRU<E> extends AbstractSet<E> implements SortedSet<E> {
         return null;	
 	}
 	
+	private E encontrar(int i) {
+        Iterator<E> it = iterator();
+        int contador = 0;
+        while (it.hasNext()) {
+            E e = it.next();
+            if (contador == i) {
+                return e;
+            }
+            contador++;
+        }
+        return null;
+	}
+	
 	@Override
 	 public E first(){
-		return null; 
+		return encontrar(inicial); 
 	 }
 	
 	@Override
 	 public E last(){
-		return null; 
+		return encontrar(fin); 
 	 }
 	
 	@Override
@@ -73,14 +93,15 @@ public class ConjuntoLRU<E> extends AbstractSet<E> implements SortedSet<E> {
     	return null;
     }
 
+
 	@Override
-	public Iterator<E> iterator() {
+	public SortedSet<E> headSet(E toElement) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public SortedSet<E> headSet(E toElement) {
+	public Iterator<E> iterator() {
 		// TODO Auto-generated method stub
 		return null;
 	}
