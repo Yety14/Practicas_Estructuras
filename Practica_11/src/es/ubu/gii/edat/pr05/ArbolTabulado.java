97d85287915231cca26160c79f8ab0f05f99a769
Guillermo <guillermo.lopzap@gmail.com>
2025-05-06 19:33:43 +0200

    yoksekolega

--- Practica_11/src/es/ubu/gii/edat/pr05/ArbolTabulado.java
+++ Practica_11/src/es/ubu/gii/edat/pr05/ArbolTabulado.java
@@ -1,5 +1,60 @@
 package es.ubu.gii.edat.pr05;
 
-public class ArbolTabulado {
+import java.util.AbstractMap;
+import java.util.List;
+import java.util.Set;
+
+public class ArbolTabulado<E> extends AbstractMap<E, E> {
+
+	public ArbolTabulado() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public ArbolTabulado(int initSize) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public E put(E hijo, E padre) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public E remove(Object objeto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public List<E> descendants(E elemento){
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public List<E> ancestors(E elemento){
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public int depth(E elemento) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public int height(E elemento) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public List<E> breadthTraverse(){
+		// TODO Auto-generated method stub
+        return null;
+	}
+
+	@Override
+	public Set<Entry<E, E>> entrySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 
 }
